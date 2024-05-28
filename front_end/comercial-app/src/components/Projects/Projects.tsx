134f3ebedc9a0a46acbc9b9828e2b628b2ed3bd2
root <root@DESKTOP-840JCIJ.localdomain>
2024-05-28 14:37:19 -0300

    Added Elena's Bakery project, minor fixes

--- front_end/comercial-app/src/components/Projects/Projects.tsx
+++ front_end/comercial-app/src/components/Projects/Projects.tsx
@@ -31,18 +31,20 @@ export default function Projects() {
                   Bootcamp con otras 7 personas. Incluye formularios controlados
                   de login con autentificación, contacto y checkout, carrito de
                   compras, filtros combinados controlados, panel de
-                  administrador y checkout con MercadoPago.
+                  administrador y checkout con MercadoPago. Completamente diseñado en Figma.
                 </p>
                 <div className="flex flex-row justify-evenly mt-5 mb-5">
                   <a
                     href="http://athens-theta.vercel.app/"
                     className="text-[#2d2e32] flex flex-row text-md"
+                    target="_blank"
                   >
                     Live Demo <BiLinkExternal size={25} />
                   </a>
                   <a
                     href="https://github.com/mas2986/PG_Front.git"
                     className="text-[#2d2e32] flex flex-row text-md"
+                    target="_blank"
                   >
                     Github Repo <AiFillGithub size={25} />
                   </a>
@@ -67,12 +69,45 @@ export default function Projects() {
                   <a
                     href="https://github.com/ManuBeni/PI-Dogs"
                     className="text-[#2d2e32] flex flex-row text-md"
+                    target="_blank"
                   >
                     Github Repo <AiFillGithub size={25} />
                   </a>
                 </div>
               </div>
             </div>
+
+
+            <div className="flex bg-slate-50 shadow-xl flex-column flex-wrap xl:m-10 2xl:mx-[300px] rounded-2xl mt-10 m-0 mb-10 justify-center items-center ">
+              <div className="my-5 bg-no-repeat shrink-0 bg-center bg-contain bg-[url('../../projects/ElenaBakeryPhoto.png')] xl:w-[37vw] sm:w-[90%] w-[100%] h-[42vh] hover:bg-[url('../../projects/ElenaBakeryGIF.gif')] rounded-2xl" />
+
+              <div className="md:px-10  xl:basis-1/3">
+                <h2 className="text-[#2d2e32] text-center font-bold text-xl xs:mt-2 sm:mt-5 mt-0">
+                  Elena's Bakery 🧁
+                </h2>
+                <p className="text-slate-600 text-center mt-5 px-10 sm:px-10 md:px-20 xl:px-0">
+                  Web app diseñada con enfoque de diseño mobile first, con creador de tortas a pedido para emprendimiento. 
+                  Incluye diseño propio en base a los requerimientos del cliente, paleta de color adecuada al rubro gastronómico, landing page con información sobre el emprendimiento, y links de contacto. 
+                </p>
+                <div className="flex flex-row justify-evenly mt-5 mb-5">
+                  <a
+                    href="https://elena-bakery.web.app/"
+                    className="text-[#2d2e32] flex flex-row text-md"
+                    target="_blank"
+                  >
+                    Live Demo <BiLinkExternal size={25} />
+                  </a>
+                  <a
+                    href="https://github.com/ManuBeni/Eleonora_Bakery"
+                    className="text-[#2d2e32] flex flex-row text-md"
+                    target="_blank"
+                  >
+                    Github Repo <AiFillGithub size={25} />
+                  </a>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
